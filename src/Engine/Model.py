6732ce56e34215ed38c81e2b00dc2ac89ef768bb
Daniel de Waal <danieldewaal02@gmail.com>
2025-07-26 15:14:09 +0200

    updated indices on model to be a 2d array

--- src/Engine/Model.py
+++ src/Engine/Model.py
@@ -5,13 +5,13 @@ class Model: pass
 class Model:
  id: UUID
  vertices: list[Vertex3f]
- indices: list[int]
+ indices: list[list[int]]
  
- def __init__(this, id: UUID, vertices: list[Vertex3f], indices: list[int]):
+ def __init__(this, id: UUID, vertices: list[Vertex3f], indices: list[list[int]]):
   this.vertices = vertices
   this.indices = indices
   
- def from_dict(model: dict[str, str | dict[str, list[Vertex3f | int]]]) -> Model:
+ def from_dict(model: dict[str, str | list[dict[str, float], list[int]]]) -> Model:
   return Model(
    id=UUID(hex=model["id"]),
    vertices=Vertex3f.from_dicts(model["vertices"]),
